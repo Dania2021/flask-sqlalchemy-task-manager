e7b8894246a1333804435e15a036902ccf294958
Dania2021 <dania.khanam@gmail.com>
2022-12-12 02:21:41 +0000

    Add datepicker, select and collapsible initialization

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
+
+  //datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+    });
+
+   //select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects); 
+
+   //collapsible initialization
+   let collapsible = document.querySelectorAll('.collapsible');
+   M.Collapsible.init(collapsible);
+});
